968956137b7ceec6e8a96d5bb036f5cb1a8f6e3c
prachir1501 <56938140+prachir1501@users.noreply.github.com>
2021-11-29 17:12:04 +0530

    Update demo.py

--- src/demo.py
+++ src/demo.py
@@ -13,6 +13,10 @@ from mtcnn import MTCNN
 
 from keras.models import load_model
 
+from google.colab.patches import cv2_imshow
+from IPython.display import Image, display
+
+
 model = load_model("HyperFAS/model/fas.h5")
 
 
@@ -33,51 +37,66 @@ def test_one(X):
 
 def test_camera(mtcnn,index = 0):
 
-            frame=cv2.imread('new1.png')
-            image = frame
+            store={}
+            for imgPath in os.listdir('outputs'):
+              frame=cv2.imread('outputs/'+imgPath)
+              image = frame
 
-            img_size = np.asarray(image.shape)[0:2]
+              img_size = np.asarray(image.shape)[0:2]
 
-            bounding_boxes, scores, landmarks = mtcnn.detect(image)
+              bounding_boxes, scores, landmarks = mtcnn.detect(image)
 
-            nrof_faces = bounding_boxes.shape[0]
-            if nrof_faces > 0:
-                for det, pts in zip(bounding_boxes, landmarks):
+              nrof_faces = bounding_boxes.shape[0]
+              if nrof_faces > 0:
+                  for det, pts in zip(bounding_boxes, landmarks):
 
-                    det = det.astype('int32')
-                    #print("face confidence: %2.3f" % confidence)
-                    det = np.squeeze(det)
-                    y1 = int(np.maximum(det[0], 0))
-                    x1 = int(np.maximum(det[1], 0))
-                    y2 = int(np.minimum(det[2], img_size[1]-1))
-                    x2 = int(np.minimum(det[3], img_size[0]-1))
+                      det = det.astype('int32')
+                      #print("face confidence: %2.3f" % confidence)
+                      det = np.squeeze(det)
+                      y1 = int(np.maximum(det[0], 0))
+                      x1 = int(np.maximum(det[1], 0))
+                      y2 = int(np.minimum(det[2], img_size[1]-1))
+                      x2 = int(np.minimum(det[3], img_size[0]-1))
 
-                    w = x2-x1
-                    h = y2-y1
-                    _r = int(max(w,h)*0.6)
-                    cx,cy = (x1+x2)//2, (y1+y2)//2
+                      w = x2-x1
+                      h = y2-y1
+                      _r = int(max(w,h)*0.6)
+                      cx,cy = (x1+x2)//2, (y1+y2)//2
 
-                    x1 = cx - _r 
-                    y1 = cy - _r 
+                      x1 = cx - _r 
+                      y1 = cy - _r 
 
-                    x1 = int(max(x1,0))
-                    y1 = int(max(y1,0))
+                      x1 = int(max(x1,0))
+                      y1 = int(max(y1,0))
 
-                    x2 = cx + _r 
-                    y2 = cy + _r 
+                      x2 = cx + _r 
+                      y2 = cy + _r 
 
-                    h,w,c =frame.shape
-                    x2 = int(min(x2 ,w-2))
-                    y2 = int(min(y2, h-2))
+                      h,w,c =frame.shape
+                      x2 = int(min(x2 ,w-2))
+                      y2 = int(min(y2, h-2))
 
-                    _frame = frame[y1:y2 , x1:x2]
-                    score = test_one(_frame)
+                      _frame = frame[y1:y2 , x1:x2]
+                      score = test_one(_frame)
 
-                    print(score)
-                    if score > 0.95:
-                        cv2.rectangle(frame, (x1,y1) ,(x2,y2) , (0,255,0)  ,2)
-                    else:
-                        cv2.rectangle(frame, (x1,y1) ,(x2,y2) , (255,0,0)  ,2)
+                      store[imgPath]=score[0]
+            
+
+            maxKey=-1
+            maxVal=-1
+            for key in store:
+              if store[key]>maxVal:
+                maxVal=store[key]
+                maxKey=key
+            
+            ans='Most similar face is '+ maxKey + ' with a confidence value of '+str(maxVal)
+            print(ans)
+            
+
+            
+
+      
+                      
 
 
     
